37a2ffe0c1c4291e2edb6778b0bd0425f0963a4b
Viktor Pazhev <asusvikp@gmail.com>
2024-12-24 02:03:30 +0200

    Update project.asm

--- Project/project.asm
+++ Project/project.asm
@@ -3,19 +3,41 @@ MODEL small
 STACK 128
 
 .data
+paging EQU 16
 ;text DB "I have something to tell you! Today I am feeling depressed!$", 0
-text DB "I have A$"
-textPtr DW text
-strLen EQU textPtr - text - 1
+text DB "World "
+text_ptr DD text
+str_len EQU text_ptr - text
+handle DW 0
+helper_file DB "helper.txt", 0
+helper_file_ptr DD helper_file
+
+
+word_start DW 0
+word_size DW 0
+
+current_word_start DW 0
+current_word_size DW 0
+
+token_start_ptr DW ?
+token_read_count DW ?
+
+buffer DW paging DUP(?)
 
 .code
+.386
 ASSUME DS:@data
+ASSUME ES:@data
 
+;changes DL!
 is_upper MACRO char
 	;DL == not upper
-	MOV DL, char LT 'A'
-	OR DL, char GT 'Z'
-	CMP DL, 0
+	CMP char, 'A'
+	JL is_not_upper
+	CMP char, 'Z'
+	JG is_not_upper
+	XOR DL, DL ; sets ZF = 1
+is_not_upper:
 ENDM
 
 ;sets ZF to 1 if str1 == str2
@@ -38,26 +60,211 @@ check_symbol:
 str_eq_exit:
 ENDM
 
-;makes letter string in lower case
-to_lower MACRO string, len
+create_file MACRO file_ptr
+	MOV AH, 3Ch
+	LDS DX, file_ptr
+	INT 21h
+ENDM
+
+open_file MACRO file_ptr, mode
+	MOV AH, 3Dh
+	MOV AL, mode
+	LDS DX, file_ptr
+	INT 21h
+	MOV handle, AX
+ENDM
+
+; read_file MACRO file_ptr, count
+	; MOV AH, 3Fh
+	; MOV BX, handle
+	; MOV CX, count
+	; INT 21h
+; ENDM
+
+write_file MACRO string, len
+	MOV AH, 40h
+	MOV BX, handle
 	MOV CX, len
-	XOR BX, BX
+	LDS DX, string
 	
-to_lower_char:
-	CMP string[BX], 'Z'
-	JG lower
-	ADD string[BX], 32
+	INT 21h
+ENDM
+
+close_file MACRO
+	MOV AH, 3Eh
+	MOV BX, handle
+	INT 21h
+ENDM
+
+; get_next_word MACRO prefix_start, prefix_end
+	; PUSHA
+
+	; ;read file
+	; MOV AH, 3Fh
+	; MOV CX, paging
+	; MOV BX, handle
+	; INT 21h
 	
-lower:
-	INC BX
-	LOOP to_lower_char
+	; POPA
+; ENDM 
+
+get_nth_token MACRO n
+	open_file helper_file_ptr, 0 ; changes AX and DX
+	PUSH n
+	
+loop_search_token:
+	; read
+	MOV AH, 3Fh
+	MOV CX, paging
+	MOV BX, handle
+	LEA DX, buffer
+	INT 21h
+	
+	CMP AX, 0
+	JE token_not_found
+	
+	MOV DI, DX
+	POP DX
+	
+	CMP DX, 0
+	JE first_token
+	
+	MOV CX, AX
+	MOV AL, 0Ah
+	CLD
+	
+count_nl:
+	REPNE SCASB
+	JNE no_nl
+	DEC DX
+	CMP DX, 0
+	JNE count_nl
+	
+	;DX == 0 => next characters until space are the token
+	DEC CX
+	MOV token_read_count, CX
+	INC DI
+	CMP CX, 0 ; we ran out of buffer bytes
+	JNE bytes_remain
+	
+	MOV AH, 3Fh
+	MOV CX, paging
+	LEA DX, buffer
+	INT 21h
+	MOV DI, DX
+	
+	MOV token_read_count, AX
+	
+bytes_remain:
+	MOV token_start_ptr, DI
+	
+	JMP token_found
+no_nl:
+	JMP loop_search_token
+	
+first_token:
+	MOV token_start_ptr, DI
+	MOV token_read_count, AX
+	JMP token_found
+	
+token_not_found:
+	XOR DX, DX
+	MOV token_read_count, DX
+
+token_found:
+	;close_file
+ENDM
+
+; word end i is after the last character
+tokenize MACRO string, word_start_i, word_end_i
+	PUSHA
+	
+	;we have a word from AX to BX - 1
+	MOV SI, word_start_i ; current char index
+	LEA SI, string[SI] ; maybe not needed? - current char pointer
+	
+	MOV word_start, SI ;start ptr
+	;MOV current_word_start, SI
+	
+	MOV DX, word_end_i
+	SUB DX, word_start_i
+	
+	MOV word_size, DX
+	;MOV current_word_size, DX
+	XOR CX, CX
+
+search_word:
+	get_nth_token CX
+	PUSH CX
+	CMP token_read_count, 0
+	JE not_found
+	
+	; here we have a token with start token_start_ptr and currently read token_read_count bytes from file
+	CLD
+	MOV DI, token_start_ptr
+	MOV CX, token_read_count
+	CMPSB
+	JNE currently_not_found
+	
+	
+	
+	close_file
+	POP CX
+	JMP tokenize_done
+	
+currently_not_found:
+	close_file
+	POP CX
+	INC CX
+	JMP search_word
+	
+	
+	; ;check if insufficient characters
+	; CMP AX, current_word_size
+	; JL not_found
+	
+	; MOV DI, DX
+	; MOV CX, AX ; = bytes read count
+	
+	; XOR BX, BX
+	; CLD
+	
+; loop_current_page:
+	; REPE CMPSB
+	; JE were_equal
+	; CMP CX, 1
+	; JE last_difference
+	
+	; ;incorrect word
+
+; last_difference:
+	; ;reached character
+	; MOV DL, [DI]
+	; CMP DL, ' '
+	; ;JNE not_
+	
+; were_equal:
+
+; finish_comparison:
+	
+	; MOV DX, BX
+	; INC BX
+	; LOOP loop_current_page
+	
+not_found:
+	;write word 1 in file here:
+	
+tokenize_done:
+	POPA
 ENDM
 
 main:
 	MOV AX, @data
 	MOV DS, AX
 	
-	MOV CX, strLen
+	;create_file helper_file_ptr
+	
+	MOV CX, str_len
 	XOR BX, BX
 	XOR AX, AX
 	
@@ -73,26 +280,26 @@ not_upper:
 	CMP BX, AX
 	JE multiple_spaces
 	
-	;MOV DX, CX
-	;MOV CX, BX
-	;SUB CX, AX
-	;XOR SI, SI
-	
-	;MOV CX, DX
+	tokenize text, AX, BX
 
 multiple_spaces:
 	MOV AX, BX
 	INC AX
 not_space:
 	INC BX
-	LOOP split_words
 	
-right:
-	MOV AH, 02h
-	INT 21h
+	DEC CX
+	JNZ split_words ; LOOP is out of range
 	
+	; MOV DL, strLen
+	; ADD dl, '0'
+	; MOV AH, 02h
+	; INT 21h
+
 exit:
 	MOV AX, 4C00h
 	INT 21h
 
-end main
+END main
+
+;Ideas: error handling
